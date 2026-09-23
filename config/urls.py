97a4f42e18cd732557e3606fcffd2a92f4f49092
adama-savadogo <adamasavadogoel.12@gmail.com>
2026-09-24 00:09:36 +0100

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
@@ -29,5 +30,5 @@ urlpatterns = [
     # Inscription / connexion / déconnexion
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='links/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
 ]
